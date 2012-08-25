a51c2aafa61c0ccb4c7215fee8204af298eb55c7
aqisnotliquid <aqisnotliquid@gmail.com>
2012-08-25 15:09:43 -0700

    Update jquery-ui.multidatespicker.js
    Reverted Issue #23 fix code. Turns out that it broke certain parts of the onSelect function. Will refork later.

--- jquery-ui.multidatespicker.js
+++ jquery-ui.multidatespicker.js
@@ -101,7 +101,15 @@
 							
 							if(this.multiDatesPicker.maxDate && (max_date > this.multiDatesPicker.maxDate))
 								max_date = this.multiDatesPicker.maxDate;
-							
+							$this
+								.datepicker("option", "minDate", min_date)
+								.datepicker("option", "maxDate", max_date);
+						} else {
+							$this
+								.datepicker("option", "minDate", this.multiDatesPicker.minDate)
+								.datepicker("option", "maxDate", this.multiDatesPicker.maxDate);
+						}
+						/* Commenting this section out until it can be tested more
 							// issue #23
 							if(methods.compareDates($this.datepicker("option", "minDate"), min_date) !== 0) 
 								$this.datepicker("option", "minDate", min_date)
@@ -114,6 +122,7 @@
 							if(methods.compareDates($this.datepicker("option", "maxDate"), this.multiDatesPicker.maxDate) !== 0)
 								$this.datepicker("option", "maxDate", this.multiDatesPicker.maxDate);
 						}
+						*/
 						
 						if(this.tagName == 'INPUT') { // for inputs
 							$this.val(
